a7348ee8b073e85c1f8b63600ef3c8eab75043cd
Louis <karpx033@umn.edu>
2021-12-16 16:49:57 -0600

    adding character option variables as well as some future code lines that I think I'll need

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var NumberPrompt = window.prompt("How many digits would you like?","8-128");
 
+
+
+
+var lowercaseOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialCharacters = [' ' ,'!', '"', '#', '$', '%', '&', "'", '(',  ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[",']', '^', '_', "`", "{", "|", "}", "~", '"']
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +16,11 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+
+generateBtn.textContent=passwordText
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
